eee275e871c919b3de83364c84aad7698bc04f7f
GitHubWanglei <963256255@qq.com>
2016-06-13 17:28:57 +0800

    .
    .

---
+++ BarChartDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarChartDemo
+//
+//  Created by wanglei on 16/6/9.
+//  Copyright © 2016年 wanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarChartDemo/test.swift
@@ -0,0 +1,9 @@
+//
+//  test.swift
+//  BarChartDemo
+//
+//  Created by wanglei on 16/6/9.
+//  Copyright © 2016年 wanglei. All rights reserved.
+//
+
+import Foundation
